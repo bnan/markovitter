3672332815be9464ef95fb7138d101b95cfea893
José Pedro Duarte <josepduarte66@gmail.com>
2018-02-10 19:42:15 +0000

    Persist previous processed twitter profiles to MongoDB

--- api.py
+++ api.py
@@ -1,15 +1,21 @@
 from flask import Flask
 from scrape import scrape
 from markov import train, generate
+from pymongo import MongoClient
 
 app = Flask(__name__)
-models = {}
+
+client = MongoClient()
+db = client.test_database
+
+tweets = db.tweets
 
 @app.route('/new/<username>', methods=['GET'])
 def new(username):
-    if username not in models:
-        models[username] = scrape(username)
-    model = train([e['full_text'] for e in models[username]])
+    if not tweets.find_one({"username" : username }):
+        tweets.insert_one({"username" : username, "tweets" : scrape(username)})
+    obj = tweets.find_one({"username" : username })
+    model = train([e['full_text'] for e in obj["tweets"]])
     text = generate(model)
     return ' '.join(text)
 
