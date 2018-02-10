918a8b3e1853e3734f9c7b40e2694d4f30033b1f
Fábio Maia <fabiu.maia@gmail.com>
2018-02-10 18:18:03 +0000

    Add wip client

--- api.py
+++ api.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from scrape import scrape
 from markov import train, generate
+from flask_cors import CORS
+import json
 
 app = Flask(__name__)
+CORS(app)
 models = {}
 
 @app.route('/new/<username>', methods=['GET'])
@@ -11,7 +14,8 @@ def new(username):
         models[username] = scrape(username)
     model = train([e['full_text'] for e in models[username]])
     text = generate(model)
-    return ' '.join(text)
+    return json.dumps({ 'message': ' '.join(text) })
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=1337)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=1337)
+
